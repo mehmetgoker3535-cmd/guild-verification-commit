cf368cd310d7fd5c70c6265c7f430e6ca1e04dad
Zer <mehmetgoker3535@gmail.com>
2026-04-29 08:12:10 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T05:12:06.309Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-04-29T05:12:08.173Z
+Commit: Fix broken links in documentation
