9add0ffff284fe0210508d2dc03c5741de904cee
Zer <mehmetgoker3535@gmail.com>
2026-04-29 08:12:15 +0300

    Add dark mode support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T05:12:04.471Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-04-29T05:12:13.781Z
+Commit: Add dark mode support
