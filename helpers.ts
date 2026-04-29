9357e66c50393e8dbaf5b0d8d75634d7b7c8ce85
Zer <mehmetgoker3535@gmail.com>
2026-04-29 08:11:34 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T05:11:28.307Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-04-29T05:11:32.369Z
+Commit: Resolve timezone issues in date handling
