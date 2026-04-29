6eebc53ac7eb32e3f81176434f2d8c02f75461ad
Zer <mehmetgoker3535@gmail.com>
2026-04-29 08:09:26 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T05:08:37.644Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-04-29T05:09:24.090Z
+Commit: Fix broken links in documentation
