f4077428b5ce00e8e91a5f767b133bfbed047f82
Zer <mehmetgoker3535@gmail.com>
2026-04-29 08:14:01 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T05:13:46.184Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-04-29T05:13:59.232Z
+Commit: Add export functionality for reports
